1d587ba8850ce3ea06f2d276b778281e0249a135
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-11-05 20:16:16 +0200

    add complexity

--- Ex1/advancedClassificationLoop.c
+++ Ex1/advancedClassificationLoop.c
@@ -2,21 +2,25 @@
 #include <stdio.h>
 #define true 1
 #define false 0
-
-int isPalindrome(int n) {
+//O(log(number)) base 10
+int isPalindrome(int number)
+{
 	int palindrome = 0;
-	int temp = n;
+	int temp = number;
 	do
 		palindrome = palindrome * 10 + temp % 10;
-	 while (temp /= 10);
-	 return (n == palindrome) ? true : false;
+	while (temp /= 10);
+	return (number == palindrome) ? true : false;
 }
-int isArmstrong(int n) {
-	int nDigits = floor(log10(abs(n))) + 1;
-	int temp = n;
+//O(log(number)) base 10
+int isArmstrong(int number)
+{
+	number = abs(number);
+	int nDigits = floor(log10(abs(number))) + 1;
+	int temp = number;
 	int armsNumber = 0;
 	do
 		armsNumber += pow(temp % 10, nDigits);
-	while (temp/=10);
-	return (n == armsNumber) ? true : false;
+	while (temp /= 10);
+	return (number == armsNumber) ? true : false;
 }
