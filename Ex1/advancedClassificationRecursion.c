c9fca379e17a99a5e8b9b0ea91e00022b0d73a46
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-11-05 20:16:52 +0200

    add complexity

--- Ex1/advancedClassificationRecursion.c
+++ Ex1/advancedClassificationRecursion.c
@@ -3,11 +3,13 @@
 #define true 1
 #define false 0
 //O(log(number)) base 10
-int isPalindrome(int number) {
+int isPalindrome(int number)
+{
 	return (reversNumber(number) == number) ? true : false;
 }
 //O(log(number)) base 10
-int reversNumber(int number) {
+int reversNumber(int number)
+{
 	if (!number)
 		return 0;
 	return (number % 10) * pow(10, floor(log10(number))) + reversNumber(number / 10);
@@ -18,7 +20,6 @@ int isArmstrong_t(int number, int digit)
 	if (!number)
 		return 0;
 	return pow(number % 10, digit) + isArmstrong_t(number / 10, digit);
-
 }
 //O(log(number)) base 10
 int isArmstrong(int number)
--- Ex1/basicClassification.c
+++ Ex1/basicClassification.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #define true 1
 #define false 0
-const int Factorial[] = { 1,1,2,6,24,120,720,5040,40320,362880 };
-//O(log(n)) base 10 n is a digit of number 
-int isStrong(int n) {
+const int Factorial[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+//O(log(n)) base 10 n is a digit of number
+int isStrong(int n)
+{
 	int factSum = 0;
 	int temp = n;
 	do
@@ -12,8 +13,10 @@ int isStrong(int n) {
 	return (factSum == n) ? true : false;
 }
 //O(n)
-int isPrime(int n) {
-	if (n == 1)return true;
+int isPrime(int n)
+{
+	if (n == 1)
+		return true;
 	for (int i = 2; i <= n / 2; ++i)
 		if (n % i == 0)
 			return false;
