d03cea362e9e6081354ef71e45e4d337d6c45e39
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-11-05 21:28:18 +0200

    add include NumClass

--- Ex1/advancedClassificationRecursion.c
+++ Ex1/advancedClassificationRecursion.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <math.h>
+#include "NumClass.h"
 #define true 1
 #define false 0
-//O(log(number)) base 10
-int isPalindrome(int number)
-{
-	return (reversNumber(number) == number) ? true : false;
-}
 //O(log(number)) base 10
 int reversNumber(int number)
 {
 	if (!number)
 		return 0;
 	return (number % 10) * pow(10, floor(log10(number))) + reversNumber(number / 10);
+}
+//O(log(number)) base 10
+int isPalindrome(int number)
+{
+	return (reversNumber(number) == number) ? true : false;
 }
 //O(log(number)) base 10
 int isArmstrong_t(int number, int digit)
