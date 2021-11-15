bc293941edfa1e44f8cbbc6d25428ca734784591
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2021-11-15 18:06:11 +0200

    new files

--- Ex2/my_mat.h
+++
@@ -1,6 +0,0 @@
-
-
-void fiilGraph(int (*)[10][10]);
-int Route(int , int , int (*)[10][10], int (*)[10]);
-void FloydWarshall(int (*)[10][10], int );
-void Initialize(int (*)[10][10], int (*)[10][10]);
